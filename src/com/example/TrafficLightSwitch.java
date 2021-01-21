055ae1a9eb747447d6498866137ddd1fea9fc5b7
Mohammedshaksho <mohammed.shakshouki@ocsbstudent.ca>
2021-01-21 18:33:40 +0000

    Section 3

--- src/com/example/TrafficLightSwitch.java
+++ src/com/example/TrafficLightSwitch.java
@@ -4,8 +4,20 @@ package com.example;
 public class TrafficLightSwitch {
 
     public static void main(String args[]) {
-
-       
-    }
-
-}
+Scanner acan = new Scanner(System.in);
+       System.out.println("Enter a colour code: ");
+    
+        int currentColour= acan.nextInt(); 
+        switch(currentColour) (
+            case l:
+             System.out.println("Next traffic ligh is green");
+            break;
+        case 2:   
+             System.out.println("Next traffic ligh is Yellow");
+            break;
+        case 3:
+             System.out.println("Next traffic ligh is Red");
+             scan.nextInt();   
+        default:     
+            System.out.println("Invalid Colour");
+            
